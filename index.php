c3becef4e840d45fb8e31ee8fbf8e69fd21e1da0
danielanthonydelacruz <anthonydelac1602@gmail.com>
2025-11-13 18:59:47 +0800

    php files added

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+
+<?php
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portfolio</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="/Images/image1.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
@@ -24,55 +31,7 @@
 
    
 
-    <div class="home-page">
-
-        <div class="text">
-        <p><h4>I'm Daniel Anthony P. Dela Cruz</h4>3rd Year collage student in Samar State University
-        and currently taking the Bachelor of Science In Information Technology</p>
-        </div>
-    </div>
-
-
-
-
-
-
-    
-     <div id="About" class="Aboutme">
-        <h1>About me</h1>
-    </div>
-
-
-
-
-
-
-
-
-
-
-    <div id="Contact" class="Contactme">
-        <h1>Contact Me</h1>
-
-        <section class="mt-5"> 
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, reprehenderit.</p>
-        </section>
-    </div>
-    
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
-    <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
